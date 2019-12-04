fdcfe3f056b2338b08d4c199cb24f82ffab532de
Andrew Okoye <andrewinsoul@gmail.com>
2019-12-04 12:44:47 +0100

    setup environment for smooth running of the application

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DB_HOST,
+    logging: false,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    dialect: 'postgres',
+    use_env_variable: process.env.DATABASE_URL,
+  },
+  production: {
+    dialect: 'postgres',
+    use_env_variable: process.env.DATABASE_URL,
+  },
+};
